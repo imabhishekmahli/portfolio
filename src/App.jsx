e695944eecff3bf3129c0089c154fa63d9bab702
imabhishekmahli <abhi8h3k@gmail.com>
2026-07-12 11:58:23 +0530

    hero component is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
+import Home from "./pages/Home";
+
 function App() {
-  return (
-    <div className="min-h-screen bg-slate-950 flex items-center justify-center">
-      <h1 className="text-6xl font-bold text-cyan-400">
-        Tailwind is Working 🚀
-      </h1>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
